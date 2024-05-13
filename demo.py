a1c4616b2279a618f263a9497ed8c109d662ab3c
Vadim Kantorov <vadimkantorov@gmail.com>
2024-05-13 17:44:18 +0200

    ...

--- demo.py
+++ demo.py
@@ -3,13 +3,15 @@ import json
 
 import nanojekyll
 
+global_variables = ['site', 'page', 'layout', 'theme', 'content', 'paginator', 'jekyll'] # https://jekyllrb.com/docs/variables/
+
 output_dir = '_site'
 
 layouts_dir = '_layouts'
 includes_dir = '_includes'
 icons_dir = '_includes/social-icons'
 
-includes_basenames = ['custom-head.html', 'footer.html', 'head.html', 'social.html', 'disqus_comments.html',  'google-analytics.html',  'header.html',  'social-item.html',  'svg_symbol.html']
+includes_basenames = ['footer.html', 'head.html', 'custom-head.html', 'social.html', 'social-item.html', 'svg_symbol.html', 'google-analytics.html',   'header.html', 'disqus_comments.html']
 
 icons_basenames = ['devto.svg', 'flickr.svg', 'google_scholar.svg', 'linkedin.svg', 'pinterest.svg', 'telegram.svg', 'youtube.svg', 'dribbble.svg', 'github.svg', 'instagram.svg', 'mastodon.svg', 'rss.svg', 'twitter.svg', 'facebook.svg', 'gitlab.svg', 'keybase.svg', 'microdotblog.svg', 'stackoverflow.svg', 'x.svg']
 
@@ -37,11 +39,42 @@ posts = {
 
 ctx = json.load(open('context.json'))
 
-jek = nanojekyll.NanoJekyll(includes = includes | icons, layouts = layouts) 
-layouts = {os.path.splitext(basename)[0] : nanojekyll.NanoJekyll.read_template(os.path.join(layouts_dir, basename)) for basename in layouts_basenames} 
+def read_template(path, front_matter_sep = '---\n'):
+    front_matter, template = '', ''
+    with open(path) as f:
+        line = f.readline()
+        if line == front_matter_sep:
+            front_matter += front_matter_sep
+            while (line := f.readline()) != front_matter_sep:
+                front_matter += line
+            front_matter += front_matter_sep
+        else:
+            template += line
+        template += f.read()
+    return front_matter, template
+
+def extract_layout_from_frontmatter(frontmatter):
+    return ([line.split(':')[1].strip() for line in frontmatter.splitlines() if line.strip().replace(' ', '').startswith('layout:')] or [None])[0]
+    
+def render(cls, ctx = {}, layout = '', content = '', layouts = {}): # https://jekyllrb.com/docs/rendering-process/
+    while layout:
+        frontmatter, template = [l for k, l in layouts.items() if k == layout or os.path.splitext(k)[0] == layout][0] 
+        content = cls(ctx | dict(content = content)).render(layout)
+        layout = extract_layout_from_frontmatter(frontmatter)
+    return content
+
+###############
+
 icons = {os.path.join(os.path.basename(icons_dir), basename) : open(os.path.join(icons_dir, basename)).read() for basename in icons_basenames} 
-includes = {basename: nanojekyll.NanoJekyll.read_template(os.path.join(includes_dir, basename)) for basename in includes_basenames}
-includes |= {os.path.splitext(k)[0] : v for k, v in includes.items()}
+
+templates_layouts = {os.path.splitext(basename)[0] : read_template(os.path.join(layouts_dir, basename)) for basename in layouts_basenames} 
+templates_includes = {basename: read_template(os.path.join(includes_dir, basename)) for basename in includes_basenames}
+templates_pages = {input_path: read_template(input_path) for input_path in pages}
+templates_posts = {input_path: read_template(input_path) for input_path in posts}
+templates_assets = {input_path : read_template(input_path) for input_path in dynamic_assets}
+
+templates_all = (templates_includes | templates_layouts | templates_pages | templates_posts | templates_assets)
+cls = nanojekyll.NanoJekyllTemplate.makecls({k : v[1] for k, v in templates_all.items()}, includes = templates_includes | icons, global_variables = global_variables)
 
 os.makedirs(output_dir, exist_ok = True)
 print(output_dir)
@@ -52,11 +85,9 @@ for input_path, output_path in static_assets.items():
         content = g.read()
         f.write(content)
     print(output_path)
-for input_path, output_path in list(dynamic_assets.items()) + list(pages.items()) + list(posts.items()):
+for input_path, output_path in list(pages.items()) + list(dynamic_assets.items()) + list(posts.items()):
     output_path = os.path.join(output_dir, output_path or input_path)
     os.makedirs(os.path.dirname(output_path), exist_ok = True)
     with open(output_path, 'w') as f:
-        frontmatter, template = jek.read_template(input_path)
-        layout = jek.extract_layout_from_frontmatter(frontmatter)
-        f.write(jek.render(ctx = ctx, template = template, layout = layout))
+        f.write(render(cls, ctx, content = content, layout = input_path, layouts = templates_all))
     print(output_path)
